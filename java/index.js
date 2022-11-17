616d6fca7ac011c4ab3a75bae721be747a10d2ba
Quinton Brockman <qbrockman.prsvr@gmail.com>
2022-11-16 21:53:30 -0500

    11.16.22

---
+++ java/index.js
@@ -0,0 +1,22 @@
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
+
+
+let input = document.getElementById("myBox")
+
+myBox.addEventListener("click", function(), {
+    console.log("I want to open the box!")
+})
+
+let myleads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+
+mylead.push(inputEl.value)
+    console.log(myleads)
+})    
