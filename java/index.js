25e177b2a4bd40ea47df288f0ac51e78783c54f1
Quinton Brockman <qbrockman.prsvr@gmail.com>
2022-12-06 21:58:32 -0500

    12.06.22

--- java/index.js
+++ java/index.js
@@ -1,49 +1,13 @@
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
-})
-
-
-let input = document.getElementById("myBox")
-
-myBox.addEventListener("click", function(), {
-    console.log("I want to open the box!")
-})
-
-let myleads = []
+let myleads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulel = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-
-mylead.push(inputEl.value)
+    mylead.push(inputEl.value)
     console.log(myleads)
 })    
 
 for (let i = 0; i < myleads.length; i++) {
-    console.log(myLeads[i])
-}
-
-const add-el = document.getElementById("add-el")
-
-function add(){
-addel.textContent 
-}
-
-console.log("add-el")
-
-//render color-el in javascript
-
-const colorEl = document.getElementById("color-el")
-
-function color(){
-colorEl.textContent
-}
-
-
-let fruit = ["apple", "orange", "pear", "strawberry", "banana"]
-
-for (let i = 0; i < fruit.length; i++){
-    console.log(fruit[i])
+    ulel.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
